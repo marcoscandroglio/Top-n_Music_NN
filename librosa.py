ab8f3bebf74d98f1ffa0efd61b2712e18bdf7ebe
Kyle <donovaky@oregonstate.edu>
2023-10-26 14:04:40 +0900

    Converted to Mel-spectrogram and saves files as text

--- librosa.py
+++ librosa.py
@@ -1,13 +1,54 @@
-# import os
-# import librosa
-# import librosa.display
-# import IPython.display as ipd
-# import numpy as np
-# import matplotlib.pyplot as plt
+import os
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_spectrogram():
-    pass
+# function to plot the spectrogram
+def plot_spectrogram(Y, sr, hop_length, y_axis="linear"):
+    plt.figure(figsize=(25, 10))
+    librosa.display.specshow(Y,
+                             sr=sr,
+                             hop_length=hop_length,
+                             x_axis="time",
+                             y_axis=y_axis)
+    plt.colorbar(format="%+2.f")
+
+# function to process audio/generate spectrograms
+def process_audio_file(audio_file):
+    # load audio file with Librosa, limiting the duration to first 30 seconds
+    # if we want a selection which is 30 seconds from the middle and not the beginning,
+    # then I will have to read more documentation
+    y, sr = librosa.load(audio_file, duration=30.0)
+
+    FRAME_SIZE = 2048
+    HOP_SIZE = 512
+
+    # extract Short-Time Fourier Transform
+    # despite the single eltter variable names, this is how the documentation does it
+    S = librosa.feature.melspectrogram(y=y, sr=sr,  n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
+
+    S_db = librosa.power_to_db(S, ref=np.max)
+
+    spectrogram_file = os.path.splitext(audio_file)[0] + '_mel_spectrogram.txt'
+    np.savetxt(spectrogram_file, S_db, fmt='%.2f', delimiter=',', comments='# ')
 
 
-def process_audio():
-    pass
+    # plot spectrograms
+    plot_spectrogram(S_db, sr, HOP_SIZE)
+    plt.title(f'Mel-Spectrogram for {os.path.basename(audio_file)} (30 seconds)')
+
+# set the var audio_directory, x Directory containing some of my audio files on desktop
+# SWITCH THIS
+audio_directory = "/Users/kyledonovan/Desktop/musicForLibrosa"
+
+# process all audio files in the folder
+# edit to individual if individual file processing desired
+for root, dirs, files in os.walk(audio_directory):
+    for file in files:
+        if file.lower().endswith(('.wav', '.mp3', '.au')):
+            audio_file = os.path.join(root, file)
+            process_audio_file(audio_file)
+
+# show the spectrograms
+plt.show()
+
