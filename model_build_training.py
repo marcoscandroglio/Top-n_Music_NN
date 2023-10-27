44b869dc6ad83088d21a6c00148486030fb3d2af
pdhopkins <hopkinsp@oregonstate.edu>
2023-10-27 01:40:12 -0600

    Improves the model, adds comments, and creates a flag system to demonstrate training using the MNIST dataset.

--- model_build_training.py
+++ model_build_training.py
@@ -13,6 +13,9 @@ import keras
 from keras import layers
 import sys
 
+# Demo training flag set to true to demonstrate training on the MNIST dataset
+DEMO_TRAINING = True
+
 
 def build_model():
     """
@@ -20,24 +23,67 @@ def build_model():
     :return: returns a Keras Model object, that can then be used for training
     """
     number_of_genres = 10
-    input = keras.Input(shape=(None, None, 1))
-    output = layers.Rescaling(scale=1/100)(input)
-    output = layers.Conv2D(filters=32, kernel_size=(4, 4))(output)
-    output = layers.GlobalAvgPool2D()(output)
-    output = layers.Dense(number_of_genres)(output)
+    # Shape of MNIST is 28 by 28, will update with project shape
+    input = keras.Input(shape=(28, 28, 1))
+    # Rescaling puts everything in the range of [0, 1]
+    output = layers.Rescaling(scale=1/255)(input)
+    # Convolutional layer makes numerous levels of the tensors
+    # Relu activation is a rectified logic unit - makes negatives go to zero
+    output = layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu")(output)
+    # Average pooling layer pools every 2x2 to its average
+    output = layers.AveragePooling2D(pool_size=(2, 2))(output)
+    output = layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu")(output)
+    output = layers.AveragePooling2D(pool_size=(2, 2))(output)
+    # Flatten takes the entire system down to a 1D tensor
+    output = layers.Flatten()(output)
+    # Dropout randomly sets values to zero, to help with overfitting
+    output = layers.Dropout(0.1)(output)
+    # The last dense layer provides the "output" of 10 nodes
+    output = layers.Dense(number_of_genres, activation="softmax")(output)
     return keras.Model(input, output)
 
 
 def main():
-    path_to_dataset = sys.argv[1]
-    # Probably need path to validation dataset as well
-    genre_dataset = tf.data.Dataset.load(path_to_dataset)
-    model = build_model()
-    model.compile(optimizer="rmsprop",
-                  loss="sparse_categorical_crossentropy",
-                  metrics=[keras.metrics.SparseCategoricalAccuracy(name="Accuracy")])
-    model.fit(genre_dataset, epochs=10)  # probably need validation_data=
-    model.save("model_data.keras")
+    if DEMO_TRAINING is True:
+        # Load in MNIST data as both training and testing sets
+        (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+        train_labels = train_labels[:5000]
+        test_labels = test_labels[5000:]
+
+        train_images = train_images[:5000]
+        test_images = test_images[5000:]
+
+        model = build_model()
+        # Compile model with RMSprop omtimizer, and using sparse categorical
+        # crossentropy for the loss function. This is because of the use
+        # of integers for categories, rather than one-hot
+        model.compile(optimizer="RMSprop",
+                      loss="sparse_categorical_crossentropy",
+                      metrics=[keras.metrics.SparseCategoricalAccuracy(name="Accuracy")])
+        # Fit the model over 10 epochs, validating each time
+        model.fit(train_images,
+                  train_labels,
+                  epochs=10,
+                  validation_data=(test_images, test_labels))
+        # Saves the model data to load back in later, or to be used in predictions
+        model.save("D:/model_data.keras")
+
+    else:
+        path_to_dataset = sys.argv[1]
+        path_to_val_set = sys.argv[2]
+        # generate the genre and validation datasets
+        genre_dataset = tf.data.Dataset.load(path_to_dataset)
+        val_dataset = tf.data.Dataset.load(path_to_val_set)
+        model = build_model()
+        model.compile(optimizer="RMSprop",
+                      loss="sparse_categorical_crossentropy",
+                      metrics=[keras.metrics.SparseCategoricalAccuracy(name="Accuracy")])
+        model.fit(genre_dataset,
+                  epochs=10,
+                  validation_data=val_dataset)
+
+        model.save("D:/model_data.keras")
 
 
 if __name__ == "__main__":
