72b4983f1dde17ec6bdd414bce2565bf59458bff
marcoscandroglio <oilgordnacsocram@gmail.com>
2023-11-04 19:39:30 -0400

    conditional change to check if model exists prior to processing data

--- model_from_dataset.py
+++ model_from_dataset.py
@@ -11,28 +11,29 @@ MODEL_NAME = 'genre_model'
 SAMPLE_FILE_DIRECTORY = 'samples'
 
 
-# convert audio files to mel-spectrograms
-libc.process_audio_database(DATA_DIRECTORY)
-
-# build tensor from mel-spectrograms
-tf_data, tf_labels = dp.pre_process(DATA_DIRECTORY)
-tf_dataset = dp.create_tensorflow_dataset(tf_data, tf_labels)
-
-tf_dataset.save(TRAINING_DATASET_DIRECTORY)
-
-# display basic tensor information
-print()
-print('typical tensor element shape:')
-print(tf_dataset.element_spec[0])
-print()
-
-# train model
 if not os.path.exists(MODEL_NAME + '.keras'):
+    # convert audio files to mel-spectrograms
+    libc.process_audio_database(DATA_DIRECTORY)
+
+    # build tensor from mel-spectrograms
+    tf_data, tf_labels = dp.pre_process(DATA_DIRECTORY)
+    tf_dataset = dp.create_tensorflow_dataset(tf_data, tf_labels)
+
+    tf_dataset.save(TRAINING_DATASET_DIRECTORY)
+
+    # display basic tensor information
+    print()
+    print('typical tensor element shape:')
+    print(tf_dataset.element_spec[0])
+    print()
+
+    # train model
     mbt.train_model(TRAINING_DATASET_DIRECTORY, MODEL_NAME)
+
 else:
     print(MODEL_NAME + '.keras' + ' exists')
 
-
+# use model to create predictions
 # Check if "samples" exists
 if os.path.exists(SAMPLE_FILE_DIRECTORY) and os.path.isdir(SAMPLE_FILE_DIRECTORY):
     # List files and directories in "samples"
