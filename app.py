0372b9e01e7b399b36a830d81e931b57a53333b7
Kyle <donovaky@oregonstate.edu>
2023-11-12 22:30:47 +0900

    Added results and error pages.
    Cleaned up the app.py and added URL search. Placeholder functions for now. 
    Cleaned up the index page and provided a more uniform look.

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # function to determine the genre of the uploaded song
-def determine_genre(song):
-    # placeholder until integration with team code
-    predicted_genre = "Rock"  # placeholder for predicted genre
+def determine_genre(song_path):
+    # placeholder for determining genre
+    return "Rock"
 
-    return predicted_genre
+def process_youtube_url(url):
+    # placeholder for processing YouTube URL
+    return [("Genre1", 0.7), ("Genre2", 0.2), ("Genre3", 0.1)]
 
 @app.route('/')
 def index():
@@ -15,13 +17,27 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    song = request.files['song'] if 'song' in request.files else None
+    if 'song' in request.files:
+        # file upload
+        song = request.files['song']
 
-    if not song or song.filename == '':
-        return "No file selected"
+        if song.filename == '':
+            return render_template('error.html', error_message="No file selected for upload")
 
-    predicted_genre = determine_genre(song)
-    return f'The predicted genre of the song is: {predicted_genre}'
+        predicted_genre = determine_genre(song)
+        return render_template('results.html', predicted_genre=predicted_genre)
+
+    elif 'url' in request.form:
+        # YouTube URL input
+        url = request.form['url']
+
+        if not url:
+            return render_template('error.html', error_message="No URL provided")
+
+        result_list = process_youtube_url(url)
+        return render_template('results.html', predicted_genre=result_list[0][0])
+
+    return "Invalid request"
 
 if __name__ == '__main__':
     app.run(debug=True)
