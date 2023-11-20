a2570f59ab227892200752e95ad8e4e657c2ef9f
Kyle <donovaky@oregonstate.edu>
2023-11-19 23:19:08 -0500

    Breaking app.py more
    genre_prediction import hangs in app.py

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 from flask import Flask, render_template, request
+from genre_prediction import predict_genre
+from music_file_conversion import youtube_get_audio
+import os
 
 app = Flask(__name__)
 
-# function to determine the genre of the uploaded song
+# function to determine the genre of the uploaded song or YouTube URL
+def determine_genre(input_path_or_url):
 
-
-def determine_genre(song_path):
-    # placeholder for determining genre
-    return "Rock"
-
-
-def process_youtube_url(url):
-    # placeholder for processing YouTube URL
-    return [("Genre1", 0.7), ("Genre2", 0.2), ("Genre3", 0.1)]
-
+    result_list = predict_genre(input_path_or_url, "model_data_val1_posscale_threeconvlayer_20epoch")
+    return result_list
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/upload', methods=['POST'])
 def upload():
+    result_list = []
+
     if 'song' in request.files:
         # file upload
         song = request.files['song']
@@ -29,21 +26,28 @@ def upload():
         if song.filename == '':
             return render_template('error.html', error_message="No file selected for upload")
 
-        predicted_genre = determine_genre(song)
-        return render_template('results.html', predicted_genre=predicted_genre)
+        # save uploaded file to temp location
+        upload_path = f"temp_{song.filename}"
+        song.save(upload_path)
+
+        # use the uploaded file path in the determine_genre function
+        result_list = determine_genre(upload_path)
+
+        # remove temp file
+        os.remove(upload_path)
 
     elif 'url' in request.form:
         # YouTube URL input
         url = request.form['url']
-
+        
+        # make sure this isn't breaking the file upload and results in errors later
         if not url:
             return render_template('error.html', error_message="No URL provided")
 
-        result_list = process_youtube_url(url)
-        return render_template('results.html', predicted_genre=result_list[0][0])
+        # use the process_youtube_url function
+        result_list = determine_genre(url)
 
-    return "Invalid request"
-
+    return render_template('results.html', result_list=result_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
